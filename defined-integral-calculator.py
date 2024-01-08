0f5f510a17dc8f8ab0838160e9357b1ecfab3ee3
Anthony Alam <anthony.alam2005@gmail.com>
2024-01-08 12:51:22 -0500

    Added Trapezium and Middle Points methods to the program

--- defined-integral-calculator.py
+++ defined-integral-calculator.py
@@ -1,17 +1,14 @@
 
 from math import *
 
-#take in all necessary information in order to calculate the defined integral (the function itself and the location of the bornes)
+#take in all necessary information in order to calculate the defined integral (the function itself, the location of the bornes and the number of sub intervals desired by the user)
 function_to_integrate = input('Please input the function you want to integrate, please make sure to watch you bracket placement and use "**" instead of "^": ' )
 top_borne = int(input(f'Please input the top borne of the defined integral, in other words "x2": '))
 bottom_borne = int(input(f'Please input the bottom borne of the defined integral, in other words "x1": '))
 
-#insure that the value given for the number_of_sub_intervals is valid
-number_of_sub_intervals = int(input(f"Please input the number of sub intervals for the defined integral. Note: This value must be an even integer for Simpson's method: "))
-while (int(number_of_sub_intervals) % 2) != 0 and int(number_of_sub_intervals) != 0:
-    number_of_sub_intervals = int(input(f"Please input the number of sub intervals for the defined integral. Note: This value must be a positive and even integer for Simpson's method: "))
+number_of_sub_intervals = int(input(f"Please input the number of sub intervals for the defined integral. Note: This value must be an even integer for Simpson's method and the Trapezium method: "))
 
-def apply_simpsons_rule(function_to_integrate, top_borne, bottom_borne, number_of_sub_intervals): #applying Simpson's rule to calculate the defined integral
+def apply_simpsons_method(function_to_integrate, top_borne, bottom_borne, number_of_sub_intervals): #applying Simpson's method to calculate the defined integral
     difference_between_bornes = top_borne - bottom_borne
     size_of_sub_intervals = difference_between_bornes / number_of_sub_intervals
     accumulative_sum = 0
@@ -36,4 +33,53 @@ def apply_simpsons_rule(function_to_integrate, top_borne, bottom_borne, number_o
 
     return ((size_of_sub_intervals) / 3) * accumulative_sum
 
-print(apply_simpsons_rule(function_to_integrate, top_borne, bottom_borne, number_of_sub_intervals))
+if number_of_sub_intervals % 2 == 0: 
+    print("\nSimpson's method: ")
+    print(apply_simpsons_method(function_to_integrate, top_borne, bottom_borne, number_of_sub_intervals), "\n")
+
+
+def apply_trapezium_method(function_to_integrate, top_borne, bottom_borne, number_of_sub_intervals): #applying the Trapezium method to calculate the defined integral
+    difference_between_bornes = top_borne - bottom_borne
+    size_of_sub_intervals = difference_between_bornes / number_of_sub_intervals
+    accumulative_sum = 0
+    current_interval = bottom_borne
+    
+    for i in range(number_of_sub_intervals + 1):
+        temporary_function = function_to_integrate.replace("x", str(current_interval))
+        temporary_function = float(f"{eval(temporary_function)}")
+        
+        if i == 0 or i == number_of_sub_intervals:
+            accumulative_sum += temporary_function
+            current_interval += size_of_sub_intervals
+        else:
+            accumulative_sum += 2*(temporary_function)
+            current_interval += size_of_sub_intervals
+
+    return ((size_of_sub_intervals) / 2) * accumulative_sum
+
+if number_of_sub_intervals % 2 == 0:
+    print("Trapezium method: ")
+    print(apply_trapezium_method(function_to_integrate, top_borne, bottom_borne, number_of_sub_intervals), "\n") 
+
+
+def apply_mid_point_rule(function_to_integrate, top_borne, bottom_borne, number_of_sub_intervals): #applying the Middle Points method to calculate the defined integral
+    difference_between_bornes = top_borne - bottom_borne
+    size_of_sub_intervals = difference_between_bornes / number_of_sub_intervals
+    accumulative_sum = 0
+    current_interval = bottom_borne + (size_of_sub_intervals) / 2
+
+    for i in range(number_of_sub_intervals):
+        temporary_function = function_to_integrate.replace("x", str(current_interval))
+        temporary_function = float(f"{eval(temporary_function)}")
+
+        accumulative_sum += temporary_function
+        current_interval += size_of_sub_intervals
+
+    return size_of_sub_intervals * (accumulative_sum)
+
+if number_of_sub_intervals % 2 != 0:
+    print("\nMiddle points method: ")
+else:
+    print("Middle points method: ")
+print(apply_mid_point_rule(function_to_integrate, top_borne, bottom_borne, number_of_sub_intervals), "\n")
+
