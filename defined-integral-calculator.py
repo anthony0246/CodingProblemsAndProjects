513adb9358161ea3744e68fef2fdec44b7ee96ca
Anthony Alam <anthony.alam2005@gmail.com>
2024-01-08 11:55:56 -0500

    Initial commit - Defined integral calculator. Works well.

---
+++ defined-integral-calculator.py
@@ -0,0 +1,39 @@
+
+from math import *
+
+#take in all necessary information in order to calculate the defined integral (the function itself and the location of the bornes)
+function_to_integrate = input('Please input the function you want to integrate, please make sure to watch you bracket placement and use "**" instead of "^": ' )
+top_borne = int(input(f'Please input the top borne of the defined integral, in other words "x2": '))
+bottom_borne = int(input(f'Please input the bottom borne of the defined integral, in other words "x1": '))
+
+#insure that the value given for the number_of_sub_intervals is valid
+number_of_sub_intervals = int(input(f"Please input the number of sub intervals for the defined integral. Note: This value must be an even integer for Simpson's method: "))
+while (int(number_of_sub_intervals) % 2) != 0 and int(number_of_sub_intervals) != 0:
+    number_of_sub_intervals = int(input(f"Please input the number of sub intervals for the defined integral. Note: This value must be a positive and even integer for Simpson's method: "))
+
+def apply_simpsons_rule(function_to_integrate, top_borne, bottom_borne, number_of_sub_intervals): #applying Simpson's rule to calculate the defined integral
+    difference_between_bornes = top_borne - bottom_borne
+    size_of_sub_intervals = difference_between_bornes / number_of_sub_intervals
+    accumulative_sum = 0
+    current_interval = bottom_borne
+    is_even_interval = True
+
+    for i in range(number_of_sub_intervals + 1):
+        temporary_function = function_to_integrate.replace("x", str(current_interval))
+        temporary_function = float(f"{eval(temporary_function)}")
+        
+        if i == 0 or i == number_of_sub_intervals:
+            accumulative_sum += temporary_function
+            current_interval += size_of_sub_intervals
+        elif is_even_interval:
+            accumulative_sum += 4*(temporary_function)
+            current_interval += size_of_sub_intervals
+            is_even_interval = False
+        elif not is_even_interval:
+            accumulative_sum += 2*(temporary_function)
+            current_interval += size_of_sub_intervals
+            is_even_interval = True
+
+    return ((size_of_sub_intervals) / 3) * accumulative_sum
+
+print(apply_simpsons_rule(function_to_integrate, top_borne, bottom_borne, number_of_sub_intervals))
